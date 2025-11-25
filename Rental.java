a25cf5437c0f87fe3c9978357b9abf8d0a1cf645
jhonatanchiari <jhonatanchiari@gmail.com>
2025-11-25 20:31:29 -0300

    Commit 6

--- Rental.java
+++ Rental.java
@@ -1,45 +1,50 @@
 public class Rental {
+    private Movie movie;
+    private int daysRented;
 
-   private Movie _movie;
-   private int _daysRented;
+    public Rental(Movie movie, int daysRented) {
+        this.movie = movie;
+        this.daysRented = daysRented;
+    }
 
-   public Rental(Movie movie, int daysRented) {
-      _movie = movie;
-      _daysRented = daysRented;
-   }
+    public int getDaysRented() {
+        return daysRented;
+    }
 
-   public int getDaysRented() {
-      return _daysRented;
-   }
+    public Movie getMovie() {
+        return movie;
+    }
 
-   public Movie getMovie() {
-      return _movie;
-   }
+    // Já criado no Refactoring anterior
+    public double getCharge() {
+        double result = 0;
 
-   // ====================================
-   // NOVO MÉTODO MOVIDO + RENOMEADO
-   // ====================================
-   public double getCharge() {
-      double result = 0;
+        switch (getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                result += 2;
+                if (getDaysRented() > 2)
+                    result += (getDaysRented() - 2) * 1.5;
+                break;
 
-      switch (getMovie().getPriceCode()) {
-         case Movie.REGULAR:
-            result += 2;
-            if (getDaysRented() > 2)
-               result += (getDaysRented() - 2) * 1.5;
-            break;
+            case Movie.NEW_RELEASE:
+                result += getDaysRented() * 3;
+                break;
 
-         case Movie.NEW_RELEASE:
-            result += getDaysRented() * 3;
-            break;
+            case Movie.CHILDRENS:
+                result += 1.5;
+                if (getDaysRented() > 3)
+                    result += (getDaysRented() - 3) * 1.5;
+                break;
+        }
+        return result;
+    }
 
-         case Movie.CHILDRENS:
-            result += 1.5;
-            if (getDaysRented() > 3)
-               result += (getDaysRented() - 3) * 1.5;
-            break;
-      }
-
-      return result;
-   }
+    //  NOVO MÉTODO EXTRAÍDO
+    public int getFrequentRenterPoints() {
+        // Regra: filmes NEW_RELEASE alugados por mais de 1 dia ganham 2 pontos
+        if (getMovie().getPriceCode() == Movie.NEW_RELEASE && getDaysRented() > 1) {
+            return 2;
+        }
+        return 1;
+    }
 }
