c8596867c41ba9a290d200046cc33191b57fa4b3
jhonatanchiari <jhonatanchiari@gmail.com>
2025-11-25 21:07:40 -0300

    Commit 9

--- Rental.java
+++ Rental.java
@@ -1,4 +1,5 @@
 public class Rental {
+
     private Movie movie;
     private int daysRented;
 
@@ -15,34 +16,16 @@ public class Rental {
         return movie;
     }
 
-    // Já criado no Refactoring anterior
+    // ================================================
+    // Método agora apenas delega ao Movie
+    // ================================================
     public double getCharge() {
-        double result = 0;
-
-        switch (getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if (getDaysRented() > 2)
-                    result += (getDaysRented() - 2) * 1.5;
-                break;
-
-            case Movie.NEW_RELEASE:
-                result += getDaysRented() * 3;
-                break;
-
-            case Movie.CHILDRENS:
-                result += 1.5;
-                if (getDaysRented() > 3)
-                    result += (getDaysRented() - 3) * 1.5;
-                break;
-        }
-        return result;
+        return movie.getCharge(daysRented);
     }
 
-    //  NOVO MÉTODO EXTRAÍDO
     public int getFrequentRenterPoints() {
-        // Regra: filmes NEW_RELEASE alugados por mais de 1 dia ganham 2 pontos
-        if (getMovie().getPriceCode() == Movie.NEW_RELEASE && getDaysRented() > 1) {
+        if (movie.getPriceCode() == Movie.NEW_RELEASE &&
+                daysRented > 1) {
             return 2;
         }
         return 1;
