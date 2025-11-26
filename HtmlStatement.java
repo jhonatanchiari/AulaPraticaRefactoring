2baed93c8ad48dda7a2dc58a979508821ddb4aed
jhonatanchiari <jhonatanchiari@gmail.com>
2025-11-25 21:46:15 -0300

    Commit 17

--- HtmlStatement.java
+++ HtmlStatement.java
@@ -1,29 +1,23 @@
-import java.util.Enumeration;
-
 public class HtmlStatement extends Statement {
 
     @Override
-    public String value(Customer aCustomer) {
+    protected String headerString(Customer aCustomer) {
+        return "<H1>Rentals for <EM>" + aCustomer.getName() + "</EM></H1><P>\n";
+    }
 
-        Enumeration rentals = aCustomer.getRentals();
-        String result = "<H1>Rentals for <EM>" + aCustomer.getName() +
-                        "</EM></H1><P>\n";
+    @Override
+    protected String eachRentalString(Rental each) {
+        return each.getMovie().getTitle() + ": " +
+               each.getCharge() + "<BR>\n";
+    }
 
-        while (rentals.hasMoreElements()) {
-            Rental each = (Rental) rentals.nextElement();
-
-            result += each.getMovie().getTitle() + ": " +
-                      String.valueOf(each.getCharge()) + "<BR>\n";
-        }
-
-        result += "<P>You owe <EM>" +
-                  String.valueOf(aCustomer.getTotalCharge()) +
-                  "</EM><P>\n";
-
+    @Override
+    protected String footerString(Customer aCustomer) {
+        String result = "<P>You owe <EM>" + aCustomer.getTotalCharge() +
+                        "</EM><P>\n";
         result += "On this rental you earned <EM>" +
-                  String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
+                  aCustomer.getTotalFrequentRenterPoints() +
                   "</EM> frequent renter points<P>";
-
         return result;
     }
 }
--- Statement.java
+++ Statement.java
@@ -1,3 +1,27 @@
+import java.util.Enumeration;
+
 public abstract class Statement {
-    public abstract String value(Customer aCustomer);
+
+    // TEMPLATE METHOD
+    public String value(Customer aCustomer) {
+
+        String result = headerString(aCustomer);
+
+        Enumeration rentals = aCustomer.getRentals();
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            result += eachRentalString(each);
+        }
+
+        result += footerString(aCustomer);
+
+        return result;
+    }
+
+    // MÉTODOS ABSTRATOS — as subclasses implementarão
+    protected abstract String headerString(Customer aCustomer);
+
+    protected abstract String eachRentalString(Rental each);
+
+    protected abstract String footerString(Customer aCustomer);
 }
